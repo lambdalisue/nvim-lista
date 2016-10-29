192016a4671a7f927fc6da2e8633bca6900703c6
lambdalisue <lambdalisue@hashnote.net>
2016-10-30 04:36:08 +0900

    Enable expr mapping

--- keymap.py
+++ keymap.py
@@ -286,7 +286,7 @@ class Keymap:
 
     def _resolve(self, nvim, definition):
         if definition.expr:
-            rhs = nvim.eval(definition.rhs)
+            rhs = Keystroke.parse(nvim, nvim.eval(definition.rhs))
         else:
             rhs = definition.rhs
         if definition.noremap:
